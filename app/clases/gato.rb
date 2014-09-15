3f6a439c71d1ae958cfa502f5dbc33ad630e78c3
Ivan Rodriguez <ivan.rodriguez.o@gmail.com>
2014-09-15 17:31:37 -0500

    mongomappin weiter

--- app/clases/gato.rb
+++ app/clases/gato.rb
@@ -1,94 +1,101 @@
 #!/usr/bin/env ruby
 # gato.rb
 class Gato
-	# Mongoid para mapear
-	include Mongoid::Document
-    field :title, type: String
-    field :body, type: String
+    include MongoMapper::Document
     
-	def texto_edad()
-		texto_edad = ''
-		num_anios = @edad
-		if num_anios < 12
-			texto_edad << num_anios.to_s + ' meses'
-		else
-			num_anios = (@edad / 12).floor
-			num_meses = @edad % 12
-			if num_anios > 0
-				texto_edad << num_anios.to_s + ' anios'
-			end
-			if num_meses > 0
-				texto_edad << ' ' + num_meses.to_s + ' meses'
-			end
-		end
-	end
-	
-	def texto_sexo()
-		return 'Gato' if @macho
-		return 'Gata'
-	end
-	
-	def edad=(edad)
-		@edad = edad
-	end
-	
-	def edad
-		@edad
-	end  
-	
-	def macho=(is_macho)
-		@macho = is_macho
-	end
-	
-	def macho
-		@macho
-	end
-	
-	def apodos=(apodos)
-		@akas = apodos
-	end
-	
-	def apodos
-		@apodos
-	end  
-	
-	def nombre=(nombre)
-		@nombre = nombre
-	end
-	
-	def nombre
-		@nombre
-	end
-	
-	def color=(color)
-		@color = color
-	end
-	
-	def color
-		@color
-	end
-	
-	def raza=(raza)
-		@raza = raza
-	end
-	
-	def raza
-		@raza
-	end
-	
-	def descripcion=(descripcion)
-		@descripcion = descripcion
-	end
-	
-	def descripcion
-		@descripcion
-	end
-	
-	def historia_medica=(historia_medica)
-		@historia_medica = historia_medica
-	end
-	
-	def historia_medica
-		@historia_medica
-	end
+    key :id, Integer
+    key :nombre,  String
+    key :apodos,  String
+    key :edad,  Integer
+    key :color,  String
+    key :raza,  String
+    key :descripcion,  String
+    key :macho,  Boolean
+    key :historia_medica,  String
+
+    def texto_edad()
+        texto_edad = ''
+        num_anios = @edad
+        if num_anios < 12
+            texto_edad << num_anios.to_s + ' meses'
+        else
+            num_anios = (@edad / 12).floor
+            num_meses = @edad % 12
+            if num_anios > 0
+                texto_edad << num_anios.to_s + ' anios'
+            end
+            if num_meses > 0
+                texto_edad << ' ' + num_meses.to_s + ' meses'
+            end
+        end
+    end
+    
+    def texto_sexo()
+        return 'Gato' if @macho
+        return 'Gata'
+    end
+    
+    def edad=(edad)
+        @edad = edad
+    end
+    
+    def edad
+        @edad
+    end  
+    
+    def macho=(is_macho)
+        @macho = is_macho
+    end
+    
+    def macho
+        @macho
+    end
+    
+    def apodos=(apodos)
+        @akas = apodos
+    end
+    
+    def apodos
+        @apodos
+    end  
+    
+    def nombre=(nombre)
+        @nombre = nombre
+    end
+    
+    def nombre
+        @nombre
+    end
+    
+    def color=(color)
+        @color = color
+    end
+    
+    def color
+        @color
+    end
+    
+    def raza=(raza)
+        @raza = raza
+    end
+    
+    def raza
+        @raza
+    end
+    
+    def descripcion=(descripcion)
+        @descripcion = descripcion
+    end
+    
+    def descripcion
+        @descripcion
+    end
+    
+    def historia_medica=(historia_medica)
+        @historia_medica = historia_medica
+    end
+    
+    def historia_medica
+        @historia_medica
+    end
 end
--- app/sandbox.rb
+++ app/sandbox.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 
 require './clases/gato'
+require 'mongo_mapper'
 
+# Normal
 gato = Gato.new()
-gato.nombre
-gato.apodos = 'lester'
+gato.nombre = 'Lester'
+gato.apodos = 'lestat'
 gato.edad = 15
 gato.color = 'blanco'
 gato.raza = 'tabby'
@@ -12,6 +14,16 @@ gato.descripcion = 'tabby'
 gato.macho = false
 gato.historia_medica = 'vacunado, esterilizado'
 
+# Mogomappeado
+MongoMapper.database = 'miaudopciones'
+gato = Gato.new(:nombre => 'Lester',
+	:apodos = 'lestat',
+	:gato.edad = 15,
+	:color = 'blanco',
+	:raza = 'tabby',
+	:descripcion = 'tabby',
+	:macho = false,
+	:historia_medica = 'vacunado, esterilizado')
+gato.save!
+gato = gato.where(:name => 'Lester').first
 puts gato.inspect
-puts gato.texto_edad
-puts gato.texto_sexo
