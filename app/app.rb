ee9acf3bb824b7cfaf5fd1e0e58e91c2e796cadd
Ivan Rodriguez <ivan.rodriguez.o@gmail.com>
2014-09-16 11:49:32 -0500

    agregar gato

--- app/app.rb
+++ app/app.rb
@@ -1,87 +1,97 @@
+#!/usr/bin/env ruby
 # app.rb
 require 'sinatra'
 require 'sinatra/content_for'
 require 'mongo_mapper'
-require './clases/gato'
-require './clases/info_nosotros'
+require './models/gato'
+require './models/info_nosotros'
 
 get '/' do
-  redirect to('/gatos')
+    redirect to('/gatos')
 end
 
-# gatos  
 get '/gatos' do
-  gatos = Gato.all
-  @gatos = gatos
-  @nosotros = InfoNosotros.first
-  erb :gatos
+    gatos = Gato.all
+    @gatos = gatos
+    @nosotros = InfoNosotros.first
+    erb :gatos
 end
 
-# gatos/{id}  
-get '/gatos/:id_gato' do
-  id_gato = params[:id_gato]
-  @gato = Gato.find(id_gato)
-  @nosotros = InfoNosotros.first
-  erb :gato_info
-end
-
-# nosotros  
-get '/nosotros' do
-  @nosotros = InfoNosotros.first
-  erb :nosotros_info
-end
-
-# login  
-get '/login' do
-  'Login'
-end
-
-# gatos/agregar  
 get '/gatos/agregar' do
-  'Agregar gato'
+    @nosotros = InfoNosotros.first
+    erb :gato_agregar
 end
 
-# gatos/eliminar/{id}  
+post '/gatos/agregar' do
+    @nosotros = InfoNosotros.first
+    if params[:sexo] == '1'
+        macho = true
+    else
+        mache = false
+    end
+    gato = Gato.new(:nombre => params[:nombre],
+        :apodos => params[:alias],
+        :edad => params[:edad],
+        :color => params[:color],
+        :raza => params[:raza],
+        :descripcion => params[:descripcion],
+        :macho => macho,
+        :historia_medica => params[:historia_medica],
+        :thumbnail => params[:thumbnail])
+    gato.save
+    @resultado = 'Gato agregado'
+    erb :resultado_operacion
+end
+
+get '/gatos/:id_gato' do
+    id_gato = params[:id_gato]
+    @gato = Gato.find(id_gato)
+    @nosotros = InfoNosotros.first
+    erb :gato_info
+end
+
+get '/nosotros' do
+    @nosotros = InfoNosotros.first
+    erb :nosotros_info
+end
+
+get '/login' do
+    @nosotros = InfoNosotros.first
+    erb :login
+end
+
 get '/gatos/eliminar/:id_gato' do
   "Eliminar gato #{params[:id_gato]}"
 end
 
-# gatos/editar/{id}  
 get '/gatos/editar/:id_gato' do
   'Editar gato'
 end
 
-# adopciones/agregar  
 get '/adopciones/agregar' do
   'Agregar adpocion'
 end
 
-# adopciones/eliminar/{id}  
 get '/adopciones/eliminar/:id_gato' do
   "Eliminar adopcion #{params[:id_gato]}"
 end
 
-# formulario/editar  
 get '/formulario/editar' do
   'Editar formulario'
 end
 
-# gatos/agregar-solicitud/{id}  
 get '/gatos/agregar-solicitud/:id_gato' do
   "Agregar solicitud #{params[:id_gato]}"
 end
 
-# nosotros/editar  
 get '/nosotros/editar' do
   'Editar info nosotros'
 end
 
-# adopciones/
 get '/adopciones' do
   'Ver lista de adoptantes'
 end
 
-# adopciones/{id}
 get '/adopciones/:id_adopcion' do
   "Ver adopcion #{params[:id_adopcion]}"
 end
---
+++ doc/modelos.md
@@ -0,0 +1,10 @@
+### InfoNosotros
+<img src="http://yuml.me/diagram/plain;/class/[InfoNosotros|-nombre;-descripcion;-lat;-lon;-usuario;-hashim]">
+### Gato
+<img src="http://yuml.me/diagram/plain;/class/[Gato|-id;-nombre;-alias;-edad;-color;-raza;-descripcion;-sexo;-historia_medica;-solicitudes_adpopcion]">
+### Adpotante
+<img src="http://yuml.me/diagram/plain;/class/[Adoptante|-id;-nombres;-apellidos;-gatos;-email;-fb;-cel;-fijo;-thumbnail]">
+### Adopcion
+<img src="http://yuml.me/diagram/plain;/class/[Adopcion|-id;-fecha;-gato;-adoptante;-formulario_adopcion]">
+### FotoGato 
+<img src="http://yuml.me/diagram/plain;/class/[FotoGato|-id;-gato;-descripcion;-url]">
